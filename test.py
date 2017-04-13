c3596ee15eb39e15e1cf880201cb5f4a0a3b491c
Roo Harrigan <rharrigan@slack-corp.com>
2017-04-13 14:04:08 -0700

    asdf

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ cat_four = "franklin"
 cat_five = "archive"
 cat_six = "six"
 cat_seven = "again"
-cat_eight = "where are we nos?"
+cat_eight = "where are we now?"
 
 user_team_id = 6454444192
 whole_row = 'get'
